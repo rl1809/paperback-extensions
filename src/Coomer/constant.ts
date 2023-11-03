1f073b07d58fd3a2ff8118e3449db2918958adbd
nhant <nha.thanhnguyen@shopee.com>
2023-11-03 14:41:15 +0700

    change the domain name of coomer and kemono

--- src/Coomer/constant.ts
+++ src/Coomer/constant.ts
@@ -1,3 +1,3 @@
-export const COOMER_DOMAIN = "https://coomer.party";
+export const COOMER_DOMAIN = "https://coomer.su";
 
 export const IMG_COOMER_DOMAIN = "https://img.coomer.party/icons";
--- src/Kemono/constant.ts
+++ src/Kemono/constant.ts
@@ -1,3 +1,3 @@
-export const KEMONO_DOMAIN = "https://kemono.party";
+export const KEMONO_DOMAIN = "https://kemono.su";
 
 export const IMG_KEMONO_DOMAIN = "https://img.kemono.party/icons";

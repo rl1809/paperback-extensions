c68efa0caec27b2037084b981707c0c737de9714
nhant <nt.nha0918@gmail.com>
2023-11-14 22:20:49 +0700

    Deploy gh-pages from commit: 0370974

--- NetTruyen/index.js
+++ NetTruyen/index.js
@@ -765,7 +765,7 @@ const parseMangaDetails = ($, mangaId) => {
     const desc = $('div.detail-content > p').text();
     const status = $('ul.list-info > li.status > p.col-xs-8').text();
     const rating = parseFloat($('span[itemprop="ratingValue"]').text());
-    const authorLink = $('ul.list-info > li.author > p.col-xs-8').attr("href") || "";
+    const authorLink = $('ul.list-info > li.author > p.col-xs-8 > a').attr("href") || "";
     if (authorLink !== "") {
         tags.push({ id: authorLink, label: author });
     }
